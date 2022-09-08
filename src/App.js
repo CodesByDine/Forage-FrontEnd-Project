0da958fd4f077ccee43b5f1664ec6409181a24a9
Geraldine <dinegerald10@gmail.com>
2022-09-08 16:08:33 +0200

    Upload Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BpkCode } from 'bpk-component-code';
 import BpkButton from 'bpk-component-button';
 import BpkText from 'bpk-component-text';
+import BpkCalendar from 'bpk-component-calendar';
+import BpkInput, { INPUT_TYPES } from 'bpk-component-input';
+import format from 'date-fns/format';
 
 import STYLES from './App.scss';
 
@@ -11,7 +14,7 @@ const App = () => (
   <div className={c('App')}>
     <header className={c('App__header')}>
       <div className={c('App__header-inner')}>
-        <BpkText tagName="h1" textStyle="xxl" className={c('App__heading')}>Welcome to React + Backpack</BpkText>
+        <BpkText tagName="h1" textStyle="xxl" className={c('App__heading')}>Flight Schedule</BpkText>
       </div>
     </header>
     <main className={c('App__main')}>
@@ -23,4 +26,64 @@ const App = () => (
   </div>
 );
 
+const formatDateFull = date => format(date, 'EEEE, do MMMM yyyy');
+const formatMonth = date => format(date, 'MMMM yyyy');
+const daysOfWeek = [
+  {
+    name: 'Sunday',
+    nameAbbr: 'Sun',
+    index: 0,
+    isWeekend: true,
+  },
+  // ...
+];
+ export class Calendar extends Component {
+  constructor () {
+    super();
+
+    this.state = {
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date: null,
+        }
+    };
+  }
+
+  handleDateSelect = (date) => {
+    this.setState({
+      selectionConfiguration: {
+        type: this.props.selectionConfiguration.type,
+        date: date,
+      },
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <BpkInput
+          id='dateInput'
+          type={INPUT_TYPES.text}
+          name='date'
+          value={(this.state.selectionConfiguration.date || '').toString()}
+          placeholder='Departure date'
+        />
+        <BpkCalendar
+          id='calendar'
+          onDateSelect={this.handleDateSelect}
+          formatMonth={formatMonth}
+          formatDateFull={formatDateFull}
+          daysOfWeek={daysOfWeek}
+          weekStartsOn={1}
+          changeMonthLabel="Change month"
+          nextMonthLabel="Next month"
+          previousMonthLabel="Previous month"
+          selectionConfiguration={this.state.selectionConfiguration}
+        />
+      </div>
+    )
+  }
+}
+
 export default App;
+
